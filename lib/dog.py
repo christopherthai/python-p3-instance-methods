663016b9ee9d62a933608f7a6e9baf9f15a5d165
Christopher Thai <christopherthai.nyli@gmail.com>
2024-04-02 15:29:44 -0400

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
+
 class Dog:
     # Class body goes here
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
+    # Instance method definition
+    def sit(self):
+        print("The dog is sitting.")
+
     pass
+
+
+fido = Dog()
+fido.bark()
+# Woof!
+
+snoopy = Dog()
+snoopy.bark()
+# Woof!
